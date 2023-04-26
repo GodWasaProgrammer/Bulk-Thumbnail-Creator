b1229909b886e5a5fb28594e2cd663af1ee1e7a2
GodWasaProgrammer <bwnils@gmail.com>
2023-04-26 18:49:17 +0200

    added facial recognition

--- Program.cs
+++ Program.cs
@@ -65,6 +65,41 @@ namespace Bulk_Thumbnail_Creator
 			Logic.TextAdder(BTCSettings.TextToAdd);
 			// Logic.MemeStashDirectories(); not used at the moment but fully functional
 			Logic.AddTextComposite(BTCSettings.PositionOfText);
+
+
+			var files = Directory.GetFiles(BTCSettings.TextAddedDir, "*.*", SearchOption.AllDirectories);
+
+			Directory.CreateDirectory(BTCSettings.FaceDetectionDir);
+
+			var faceDetector = new FaceDetector(0.95f, 0.5f);
+			var painter = new Painter()
+			{
+				BoxPen = new Pen(Color.Yellow, 4),
+				Transparency = 0,
+			};
+
+			Console.WriteLine($"Processing {files.Length} images");
+
+			foreach (var file in files)
+			{
+				var bitmap = new Bitmap(file);
+				var output = faceDetector.Forward(bitmap);
+
+				foreach (var rectangle in output)
+				{
+					var paintData = new PaintData()
+					{
+						Rectangle = rectangle,
+						Title = string.Empty
+					};
+					var graphics = Graphics.FromImage(bitmap);
+					painter.Draw(graphics, paintData);
+				}
+
+				var filename = Path.GetFileName(file);
+				bitmap.Save(Path.Combine(BTCSettings.FaceDetectionDir, filename));
+				Console.WriteLine($"Image: [{filename}] --> detected [{output.Length}] faces");
+			}
 		}
 
 	}
