6e400faae88af9aaed212661c6570528c449163f
GodWasaProgrammer <bwnils@gmail.com>
2023-11-01 23:04:39 +0100

    fleshed out the logic for zipservice a bit more

--- Services/ZipService.cs
+++ Services/ZipService.cs
@@ -5,29 +5,27 @@ namespace Bulk_Thumbnail_Creator.Services
 {
     public class ZipService
     {
+        static string wwwrootloc = "/wwwroot";
+
         public static string ZipOutputDir()
         {
-            string zippedFile = "output.zip";
-
+            string zippedFile = $"{wwwrootloc}/output.zip";
             ZipFile.CreateFromDirectory(Settings.OutputDir, zippedFile);
-
             return zippedFile;
         }
 
         public static string ZipVideo()
         {
-            string zippedvideo = $"{Settings.PathToVideo}.zip";
+            string zippedvideo = $"{wwwrootloc}/{Settings.PathToVideo}.zip";
             using var zip = ZipFile.Open(zippedvideo, ZipArchiveMode.Create);
             zip.CreateEntryFromFile($"{Settings.PathToVideo}", $"{Path.GetFileNameWithoutExtension(Settings.PathToVideo)}");
-
             return zippedvideo;
         }
 
         public static string ZipTextAddedDir()
         {
-            string zippedtextAddedDir = $"{Settings.TextAddedDir}.zip";
+            string zippedtextAddedDir = $"{wwwrootloc}/{Settings.TextAddedDir}.zip";
             ZipFile.CreateFromDirectory(Settings.TextAddedDir, $"{Settings.TextAddedDir}.zip");
-
             return zippedtextAddedDir;
         }
 
