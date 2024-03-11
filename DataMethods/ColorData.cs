c24f9715cb7c196aa97a62cac8cad3132a051421
GodWasaProgrammer <bwnils@gmail.com>
2024-03-11 21:41:32 +0100

    Added a method to return you the hexadecimal of two widely varying colors

--- DataMethods/ColorData.cs
+++ DataMethods/ColorData.cs
@@ -101,77 +101,130 @@ public class ColorData
         return param;
     }
 
-static Random hueRandom = new Random(396235);
-static Random satRandom = new Random(1112434);
-static Random LightRandom = new Random(35325323);
-/// <summary>
-/// Generates Color output to be used in a PictureData Object to generate text colors
-/// </summary>
-/// <param name="InputParameter">ParamForTextcreation Object to Generate Colors for</param>
-/// <param name="currentelement">the current index of the object being passed</param>
-/// <returns>returns the ParamForTextCreation object with the modified Color Values</returns>
-public static ParamForTextCreation DecideColorGenerationAlt(ParamForTextCreation InputParameter)
-{
-    float hueFill;
-    float hueStroke;
-    float hueBorder;
-
-    bool fillOkay = false;
-    bool strokeOkay = false;
-    bool borderOkay = false;
-    int MaxHue = 360;
-    do
+    static Random hueRandom = new Random(396235);
+    static Random satRandom = new Random(1112434);
+    static Random LightRandom = new Random(35325323);
+    /// <summary>
+    /// Generates Color output to be used in a PictureData Object to generate text colors
+    /// </summary>
+    /// <param name="InputParameter">ParamForTextcreation Object to Generate Colors for</param>
+    /// <param name="currentelement">the current index of the object being passed</param>
+    /// <returns>returns the ParamForTextCreation object with the modified Color Values</returns>
+    public static ParamForTextCreation DecideColorGenerationAlt(ParamForTextCreation InputParameter)
     {
-        hueFill = hueRandom.Next(MaxHue);
-        hueStroke = hueRandom.Next(MaxHue);
-        hueBorder = hueRandom.Next(MaxHue);
+        float hueFill;
+        float hueStroke;
+        float hueBorder;
 
-        // Check if the difference between the new value and any existing value is within the range
-
-        if (Math.Abs(hueFill - hueStroke) >= 35 && Math.Abs(hueFill - hueBorder) >= 35 && Math.Abs(hueBorder - hueStroke) >= 35)
+        bool fillOkay = false;
+        bool strokeOkay = false;
+        bool borderOkay = false;
+        int MaxHue = 360;
+        do
         {
-            fillOkay = true;
-            strokeOkay = true;
-            borderOkay = true;
+            hueFill = hueRandom.Next(MaxHue);
+            hueStroke = hueRandom.Next(MaxHue);
+            hueBorder = hueRandom.Next(MaxHue);
+
+            // Check if the difference between the new value and any existing value is within the range
+
+            if (Math.Abs(hueFill - hueStroke) >= 35 && Math.Abs(hueFill - hueBorder) >= 35 && Math.Abs(hueBorder - hueStroke) >= 35)
+            {
+                fillOkay = true;
+                strokeOkay = true;
+                borderOkay = true;
+            }
+
+        } while (!fillOkay || !strokeOkay || !borderOkay);
+
+        float satFill;
+        float satStroke;
+        float satBorder;
+
+        bool satFillColorOkay = false;
+        bool satStrokeColorOkay = false;
+        bool satBorderColorOkay = false;
+
+        do
+        {
+            satFill = (float)satRandom.NextDouble() + 0.35F;
+            satStroke = (float)satRandom.NextDouble() + 0.35F;
+            satBorder = (float)satRandom.NextDouble() + 0.35F;
+
+            if (Math.Abs(satFill - satStroke) >= 0.25 && Math.Abs(satFill - satBorder) >= 0.25 && Math.Abs(satBorder - satStroke) >= 0.25)
+            {
+                satFillColorOkay = true;
+                satStrokeColorOkay = true;
+                satBorderColorOkay = true;
+            }
+
+        } while (!satFillColorOkay || !satStrokeColorOkay || !satBorderColorOkay);
+
+        float lightness;
+        do
+        {
+            lightness = (float)LightRandom.NextDouble();
+        } while (lightness < 0.25 || lightness > 0.60);
+
+        InputParameter.FillColor.SetByHSL(hueFill, satFill, lightness);
+
+        InputParameter.StrokeColor.SetByHSL(hueStroke, satStroke, lightness);
+
+        InputParameter.BorderColor.SetByHSL(hueBorder, satBorder, lightness);
+
+        return InputParameter;
+    }
+
+    static List<string> selectedColors = new List<string>();
+    public static List<MagickColor> SelectTwoDifferentColors()
+    {
+        MagickColor firstcolor;
+        MagickColor secondColor;
+
+        List<string> colorList = GetAllMagickColors();
+
+        colorList.Remove("None");
+        colorList.Remove("Transparent");
+
+        foreach (var alreadyselectedcolor in selectedColors)
+        {
+            colorList.Remove(alreadyselectedcolor);
         }
 
-    } while (!fillOkay || !strokeOkay || !borderOkay);
-
-    float satFill;
-    float satStroke;
-    float satBorder;
-
-    bool satFillColorOkay = false;
-    bool satStrokeColorOkay = false;
-    bool satBorderColorOkay = false;
-
-    do
-    {
-        satFill = (float)satRandom.NextDouble() + 0.35F;
-        satStroke = (float)satRandom.NextDouble() + 0.35F;
-        satBorder = (float)satRandom.NextDouble() + 0.35F;
-
-        if (Math.Abs(satFill - satStroke) >= 0.25 && Math.Abs(satFill - satBorder) >= 0.25 && Math.Abs(satBorder - satStroke) >= 0.25)
+        List<MagickColor> colors = new List<MagickColor>();
+        for (int i = 0; i < colorList.Count; i++)
         {
-            satFillColorOkay = true;
-            satStrokeColorOkay = true;
-            satBorderColorOkay = true;
+            if (i == 0)
+            {
+                firstcolor = new(colorList[i]);
+                selectedColors.Add(colorList[i]);
+                colors.Add(firstcolor);
+            }
+
+            if (i == colorList.Count / 2)
+            {
+                secondColor = new(colorList[i]);
+                selectedColors.Add(colorList[i]);
+                colors.Add(secondColor);
+            }
         }
+        return colors;
+    }
 
-    } while (!satFillColorOkay || !satStrokeColorOkay || !satBorderColorOkay);
-
-    float lightness;
-    do
+    private static List<string> GetAllMagickColors()
     {
-        lightness = (float)LightRandom.NextDouble();
-    } while (lightness < 0.25 || lightness > 0.60);
+        List<string> colorsList = new List<string>();
 
-    InputParameter.FillColor.SetByHSL(hueFill, satFill, lightness);
+        Type magickColorsType = typeof(MagickColors);
+        PropertyInfo[] properties = magickColorsType.GetProperties(BindingFlags.Public | BindingFlags.Static);
 
-    InputParameter.StrokeColor.SetByHSL(hueStroke, satStroke, lightness);
-
-    InputParameter.BorderColor.SetByHSL(hueBorder, satBorder, lightness);
-
-    return InputParameter;
-}
+        foreach (PropertyInfo property in properties)
+        {
+            if (property.PropertyType == typeof(MagickColor))
+            {
+                colorsList.Add(property.Name);
+            }
+        }
+        return colorsList;
+    }
 }
