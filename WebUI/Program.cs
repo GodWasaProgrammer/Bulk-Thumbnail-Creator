c1c740e89fca084061428f64f5488b6c9a77dfb4
GodWasaProgrammer <bwnils@gmail.com>
2024-03-28 01:58:38 +0100

    Implemented google login besides the general login

--- WebUI/Program.cs
+++ WebUI/Program.cs
@@ -1,10 +1,13 @@
 ﻿using BulkThumbnailCreator;
 using BulkThumbnailCreator.Interfaces;
 using BulkThumbnailCreator.Services;
+using Microsoft.AspNetCore.Authentication.Google;
+using Microsoft.AspNetCore.Components.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.FileProviders;
 using MudBlazor.Services;
+using WebUI.Areas.Identity;
 namespace WebUI;
 
 public static class Program
@@ -16,15 +19,25 @@ public static class Program
         Directory.CreateDirectory("TextAdded");
         Directory.CreateDirectory("logs");
         var builder = WebApplication.CreateBuilder(args);
+        var configuration = builder.Configuration;
         // Add configuration settings
-        builder.Configuration.AddJsonFile("appsettings.json", optional: false, reloadOnChange: true);
+        builder.Configuration.AddJsonFile("appsettings.json", optional: false);
         builder.Services.AddDefaultIdentity<IdentityUser>()
                         .AddEntityFrameworkStores<ApplicationDbContext>();
+        //GoogleOptions debugoptions = new();
+        //debugoptions.ClientId = configuration["Authentication:Google:ClientId"];
+        //debugoptions.ClientSecret = configuration["Authentication:Google:ClientSecret"];
+        builder.Services.AddAuthentication().AddGoogle(googleOptions =>
+        {
+            googleOptions.ClientId = "";
+            googleOptions.ClientSecret = "";
+        });
         // Add services
         builder.Services.AddDbContext<ApplicationDbContext>(options => options.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection")));
 
         builder.Services.AddRazorPages();
         builder.Services.AddServerSideBlazor();
+        builder.Services.AddScoped<AuthenticationStateProvider, RevalidatingIdentityAuthenticationStateProvider<IdentityUser>>();
         builder.Services.AddScoped<CreatorService>();
         builder.Services.AddScoped<JobService>();
         builder.Services.AddScoped<ILogService, LogService>();
@@ -36,6 +49,18 @@ public static class Program
 
         // Configure the HTTP request pipeline.
         if (!app.Environment.IsDevelopment())
+        {
+            app.UseExceptionHandler("/Error");
+            // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+            app.UseHsts();
+        }
+
+        // Configure the HTTP request pipeline.
+        if (app.Environment.IsDevelopment())
+        {
+            app.UseMigrationsEndPoint();
+        }
+        else
         {
             app.UseExceptionHandler("/Error");
             // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
