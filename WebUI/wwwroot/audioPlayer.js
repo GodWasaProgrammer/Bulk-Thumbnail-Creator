d9818898b48864facc24ed78b0948f391dded378
GodWasaProgrammer <bwnils@gmail.com>
2024-09-11 20:35:07 +0200

    Finished musicplayer layout and Code Section

---
+++ WebUI/wwwroot/audioPlayer.js
@@ -0,0 +1,46 @@
+﻿window.audioPlayer = {
+    player: null,
+
+    initialize: function (elementId) {
+        this.player = document.getElementById(elementId);
+    },
+
+    play: function () {
+        if (this.player) {
+            this.player.play();
+        }
+    },
+
+    pause: function () {
+        if (this.player) {
+            this.player.pause();
+        }
+    },
+
+    stop: function () {
+        if (this.player) {
+            this.player.pause();
+            this.player.currentTime = 0;
+        }
+    },
+
+    next: function (nextUrl) {
+        if (this.player) {
+            this.player.src = nextUrl;
+            this.player.play();
+        }
+    },
+
+    previous: function (previousUrl) {
+        if (this.player) {
+            this.player.src = previousUrl;
+            this.player.play();
+        }
+    },
+
+    setVolume: function (volume) {
+        if (this.player) {
+            this.player.volume = volume / 100;
+        }
+    }
+};
