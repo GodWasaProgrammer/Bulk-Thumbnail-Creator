7ae85cc7790ea5dadafc8acc94d7bc1aca54e0ef
GodWasaProgrammer <bwnils@gmail.com>
2024-09-12 13:35:05 +0200

    continued work on musicplayer

--- WebUI/wwwroot/audioPlayer.js
+++ WebUI/wwwroot/audioPlayer.js
@@ -3,18 +3,29 @@
 
     initialize: function (elementId) {
         this.player = document.getElementById(elementId);
+        if (this.player) {
+            console.log("Player initialized with ID:", elementId);
+            console.log("Current player source:", this.player.src);
+        } else {
+            console.error("Player initialization failed: Element not found");
+        }
     },
 
     play: function () {
         if (this.player) {
-            this.player.play();
+            console.log("Attempting to play");
+            this.player.play().then(() => {
+                console.log("Started Playing");
+            }).catch(error => {
+                console.error("Error playing video:", error);
+            });
+        } else {
+            console.error("Player not initialized");
         }
     },
 
     pause: function () {
-        if (this.player) {
-            this.player.pause();
-        }
+        if (this.player) this.player.pause();
     },
 
     stop: function () {
@@ -24,23 +35,34 @@
         }
     },
 
-    next: function (nextUrl) {
+    next: function (url) {
         if (this.player) {
-            this.player.src = nextUrl;
-            this.player.play();
+            console.log("Setting video source to:", url);
+            this.player.src = url;
+            this.player.load(); // Ensure the video metadata is loaded
+            this.player.play().then(() => {
+                console.log("Playing new video");
+            }).catch(error => {
+                console.error("Error playing new video:", error);
+            });
         }
     },
 
-    previous: function (previousUrl) {
+    previous: function (url) {
         if (this.player) {
-            this.player.src = previousUrl;
-            this.player.play();
+            console.log("Setting video source to:", url);
+            this.player.src = url;
+            this.player.load(); // Ensure the video metadata is loaded
+            this.player.play().then(() => {
+                console.log("Playing previous video");
+            }).catch(error => {
+                console.error("Error playing previous video:", error);
+            });
         }
     },
 
     setVolume: function (volume) {
-        if (this.player) {
-            this.player.volume = volume / 100;
-        }
+        if (this.player) this.player.volume = volume / 100;
     }
 };
+
