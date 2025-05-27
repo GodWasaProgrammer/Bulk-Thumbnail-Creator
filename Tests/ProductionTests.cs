444be1811de37b300af8c2001ff8e95cdade3a81
GodWasaProgrammer <72033235+GodWasaProgrammer@users.noreply.github.com>
2025-05-27 20:38:46 +0200

    Corrected the remaining tests that was in need of refactoring

--- Tests/ProductionTests.cs
+++ Tests/ProductionTests.cs
@@ -3,6 +3,8 @@ using BulkThumbnailCreator.Enums;
 using BulkThumbnailCreator.Interfaces;
 using BulkThumbnailCreator.PictureClasses;
 using BulkThumbnailCreator.Services;
+using BulkThumbnailCreator.Wrappers;
+using DlibDotNet;
 using ImageMagick;
 using Moq;
 
@@ -16,58 +18,60 @@ public class ProductionTests
     //    throw new NotImplementedException();
     //}
 
-    //[Fact]
-    //public void ProduceTextPictures_Produce_Text_Image_Text()
-    //{
-    //    // Arrange
-    //    // First we set up our services
-    //    var logService = new Mock<ILogService>();
-    //    var logger = logService.Object;
-    //    var jobService = new JobService();
+    [Fact]
+    public async Task ProduceTextImageAsync()
+    {
+        // Arrange
+        // First we set up our services
+        var logService = new Mock<ILogService>();
+        var logger = logService.Object;
+        var jobService = new JobService();
 
-    //    // We then set up our settings and parameters and dependencies
-    //    var settings = new Settings(logger, jobService);
+        // We then set up our settings and parameters and dependencies
+        var settings = new Settings();
 
-    //    // We dont mock this as we want to fetch some actual fonts
-    //    var dirWrapper = new DirectoryWrapper();
+        // We dont mock this as we want to fetch some actual fonts
+        var dirWrapper = new DirectoryWrapper();
 
-    //    var dg = new DataGeneration(dirWrapper);
-    //    var picData = new PictureData();
-    //    picData.OutPutType = OutputType.Main;
+        var dg = new DataGeneration(dirWrapper);
+        var picData = new PictureData();
+        picData.OutPutType = OutputType.Main;
 
-    //    // first we make an actual file to read into the memory
-    //    // for this we will utilize imagemagick
-    //    var imageMagickSettings = new MagickReadSettings
-    //    {
-    //        BackgroundColor = MagickColors.None,
-    //        Font = "Arial",
-    //        TextGravity = Gravity.Center,
-    //        Width = 800,
-    //        Height = 200,
-    //        FontPointsize = 72
-    //    };
+        // first we make an actual file to read into the memory
+        // for this we will utilize imagemagick
+        var imageMagickSettings = new MagickReadSettings
+        {
+            BackgroundColor = MagickColors.None,
+            Font = "Arial",
+            TextGravity = Gravity.Center,
+            Width = 800,
+            Height = 200,
+            FontPointsize = 72
+        };
 
-    //    var tempFilePath = "output_Unit_test.png";
+        var tempFilePath = "output_Unit_test.png";
 
-    //    using (var image = new MagickImage("label:Hello", imageMagickSettings))
-    //    {
-    //        image.Wave(PixelInterpolateMethod.Spline, 5, 100);
-    //        image.Write(tempFilePath);
-    //    }
-    //    picData.FileName = tempFilePath;
+        using (var image = new MagickImage("label:Hello", imageMagickSettings))
+        {
+            image.Wave(PixelInterpolateMethod.Spline, 5, 100);
+            image.Write(tempFilePath);
+        }
+        picData.FileName = tempFilePath;
 
-    //    // we now have an actual picture, so we set up some parameters
+        // we now have an actual picture, so we set up some parameters
 
-    //    var param1 = new ParamForTextCreation();
-    //    param1.Text = "param1";
+        var param1 = new ParamForTextCreation();
+        param1.Text = "param1";
 
+        var loadedimg = Dlib.LoadImage<RgbPixel>(tempFilePath);
 
-    //    param1.Boxes = DataGeneration.BuildDefaultBoxes();
-    //    var param2 = new ParamForTextCreation();
+        param1.Boxes = DataGeneration.BuildDefaultBoxes(loadedimg);
+        var param2 = new ParamForTextCreation();
 
-    //    // Act
-    //    Production.ProduceTextPictures()
-    //}
+        // Act
+        var prodinstance = new Production(logger);
+        await prodinstance.ProduceTextPictures(picData, settings);
+    }
 
     [Fact]
     public async Task CreateImage_ValidFile_ReturnsMagickImage()
@@ -100,47 +104,52 @@ public class ProductionTests
         };
 
         var mockLogService = new Mock<ILogService>();
+        var logserviceOb = mockLogService.Object;
         var jobService = new JobService();
-        //var settings = new Settings(mockLogService.Object, jobService);
+        var settings = new Settings();
 
-        // Act
-        // var result = await Production.CreateImage(pictureData, settings);
+        //Act'
+        var prodInstance = new Production(logserviceOb);
+        var result = await prodInstance.CreateImage(pictureData, settings);
 
-        // Assert
-        //Assert.NotNull(result);
-        //Assert.Equal(800, result.Width);
-        ////Assert.Equal(200, result.Height);
-        //Assert.IsType<MagickImage>(result);
+        //Assert
+        Assert.NotNull(result);
+        Assert.Equal(800, result.Width);
+        //Assert.Equal(200, result.Height);
+        Assert.IsType<MagickImage>(result);
 
-        //// Erase our temp file
-        //if (File.Exists(tempFilePath))
-        //{
-        //    File.Delete(tempFilePath);
-        //}
+        // Erase our temp file
+        if (File.Exists(tempFilePath))
+        {
+            File.Delete(tempFilePath);
+        }
     }
 
-    //[Fact]
-    //public async Task CreateImage_InvalidFile_LogsErrorAndReturnsNull()
-    //{
-    //    // Arrange
+    [Fact]
+    public async Task CreateImage_InvalidFile_LogsErrorAndReturnsNull()
+    {
+        // Arrange
 
-    //    // we will pass a file that doesnt exist to make sure it is handled gracefully
-    //    var pictureData = new PictureData
-    //    {
-    //        FileName = "non_existent_file.png"
-    //    };
+        // we will pass a file that doesnt exist to make sure it is handled gracefully
+        var pictureData = new PictureData
+        {
+            FileName = "non_existent_file.png"
+        };
 
-    //    var mockLogService = new Mock<ILogService>();
-    //    var jobService = new JobService();
-    //    var settings = new Settings(mockLogService.Object, jobService);
+        var mockLogService = new Mock<ILogService>();
+        var jobService = new JobService();
+        var settings = new Settings();
 
-    //    // Act
-    //    var result = await Production.CreateImage(pictureData, settings);
+        var MockLoggerObj = mockLogService.Object;
+        var prodInstance = new Production(MockLoggerObj);
 
-    //    // Assert
-    //    Assert.Null(result);
-    //    mockLogService.Verify(log => log.LogError(It.IsAny<string>()), Times.Once);
-    //}
+        // Act
+        var result = await prodInstance.CreateImage(pictureData, settings);
+
+        // Assert
+        Assert.Null(result);
+        mockLogService.Verify(log => log.LogError(It.IsAny<string>()), Times.Once);
+    }
 
     [Fact]
     public void BuildFileName_Test()
@@ -171,7 +180,7 @@ public class ProductionTests
         picDataCustom.BoxParameters.Add(param2);
 
         //Act
-       var resultMain = Production.BuildFileName(picDataMain, settings);
+        var resultMain = Production.BuildFileName(picDataMain, settings);
         var resultRandom = Production.BuildFileName(picDataRandom, settings);
         var resultMeme = Production.BuildFileName(picDataMeme, settings);
         var resultCustom = Production.BuildFileName(picDataCustom, settings);
